b498dba762cedcd9d4f142f3d4893b94909f770f
Your Name <you@example.com>
2022-04-24 20:50:09 +0700

    first commit

---
+++ Restaurant/resources/views/products/show.blade.php
@@ -0,0 +1,35 @@
+@extends('products.layout')
+   
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2> Show Restaurant</h2>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-primary" href="{{ route('products.index') }}"> Back</a>
+            </div>
+        </div>
+    </div>
+     
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Name Restaurant:</strong></br>
+                {{ $product->name }}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Address:</strong></br>
+                {{ $product->detail }}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Image:</strong></br>
+                <img src="/image/{{ $product->image }}" width="500px">
+            </div>
+        </div>
+    </div>
+@endsection
